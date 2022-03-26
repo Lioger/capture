5acddd74e36b5327fb847a6f8b132a5a62fb3aac
Nikita Stepanov <nikitastepanov@MacBook-Pro-Nikita.local>
2022-03-26 17:17:13 +0300

    Changed title and added link to email

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,21 +1,19 @@
-import { motion } from 'framer-motion';
-import Wave from './Wave';
+import { motion } from "framer-motion";
+import Wave from "./Wave";
 // Import images
-import home1 from '../img/home1.png';
+import home1 from "../img/home1.png";
 // Import styled components
-import { Section, Description, Image, Hide } from '../styles';
+import { Section, Description, Image, Hide } from "../styles";
 // Animations
-import { titleAnim, fade, photoAnim } from '../animation';
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
   return (
-    <Section className='about'>
+    <Section className="about">
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2 variants={titleAnim}>
-              We work to make
-            </motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
             <motion.h2 variants={titleAnim}>
@@ -23,15 +21,16 @@ const AboutSection = () => {
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnim}>
-              true.
-            </motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
         <motion.p variants={fade}>
-          Contact us for any photography or vigeography ideas that you have. We have professonals with amazing skills.
+          Contact us for any photography or vigeography ideas that you have. We
+          have professonals with amazing skills.
         </motion.p>
-        <motion.button variants={fade}>Contact Us</motion.button>
+        <motion.a variants={fade} href="mailto:nickstepanovdev@gmail.com">
+          Contact Us
+        </motion.a>
       </Description>
       <Image>
         <motion.img variants={photoAnim} src={home1} alt="Guy with a camera" />
